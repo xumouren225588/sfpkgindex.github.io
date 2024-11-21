cef94aab2ce84d24bef91ec2de59713669802d14
xumouren225588 <xumouren225588@163.com>
2024-11-21 12:15:07 +0800

    Update build.py

--- build.py
+++ build.py
@@ -16,9 +16,37 @@ readme_content += "- 应用名称\t应用编号\n"
 # 遍历JSON数据，格式化字符串
 for key, value in data.items():
     readme_content += f"- {key}\t{value}\n"
+def convert_tabs_to_spaces(text):
+    
 
-# 将格式化后的内容写入到README.md文件中
-with open(readme_file_path, 'w', encoding='utf-8') as file:
-    file.write(readme_content)
+    lines = text.split('\n')
+    new_lines = []
 
-print("README.md文件已更新。")
+    for line in lines:
+        tab_positions = []
+        index = 0
+        while True:
+            index = line.find('\t', index)
+            if index == -1:
+                break
+            tab_positions.append(index)
+            index += 1
+
+        new_line = line
+        for pos in tab_positions:
+            spaces_num = 8 - (pos % 8)
+            new_line = new_line.replace('\t', ' '*spaces_num, 1)
+
+        new_lines.append(new_line)
+
+    new_text = "\n".join(new_lines)
+
+    with open("README.md", "w", encoding='utf-8') as f:
+        f.write(new_text)
+
+if __name__ == "__main__":
+    convert_tabs_to_spaces(readme_content)
+    # 将格式化后的内容写入到README.md文件中
+
+
+    print("README.md文件已更新。")
