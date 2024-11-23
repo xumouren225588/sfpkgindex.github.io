602a982e5f4ca35b788a6b8d1a741cbff5e4648b
xumouren225588 <xumouren225588@163.com>
2024-11-23 10:21:45 +0800

    Update build.py

--- build.py
+++ build.py
@@ -1,12 +1,19 @@
 import json
 
+
+def generate_checkbox_html(json_data):
+    checkbox_html = ""
+    for key, value in json_data.items():
+        checkbox_html += f'        <input type="checkbox" id="{value}" value="{value}">{key}<br>'
+    return checkbox_html
+
+
 # 假设你的JSON文件名为data.json
 json_file = 'test.json'
 
 # 读取JSON文件
 with open(json_file, 'r', encoding='utf-8') as file:
     data = json.load(file)
-
 # 初始化两个列表，分别存储键和值
 keys = []
 values = []
@@ -26,3 +33,27 @@ with open('README.md', 'w', encoding='utf-8') as md_file:
     # 遍历键和值列表，写入表格行
     for key, value in zip(keys, values):
         md_file.write(f'| {key} | {value} |\n')
+# 生成复选框的HTML代码
+checkbox_html = generate_checkbox_html(data)
+
+html_template = f"""<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-height=1.0">
+    <title>Checkbox to JSON and Download</title>
+</head>
+
+<body>
+    <form id="myForm">
+{checkbox_html}
+        <input type="submit" value="提交">
+    </form>
+
+    <script>
+        document.getElementById("myForm").addEventListener("submit", function (event) {{
+            event.preventDefault();
+
+            var selectedItems = [];
+            var checkboxes = document.querySelectorAll('inp
